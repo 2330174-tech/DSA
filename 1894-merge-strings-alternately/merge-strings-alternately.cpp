0593e926b5678726882b5cbea39014d0979f0ac9
2330174-tech <2330174@kiit.ac.in>
2026-08-15 02:42:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (64.15%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+        string merge = "";
+
+        int i = 0;
+        int j = 0;
+
+        while (i < word1.length() || j < word2.length()) {
+
+            if (i < word1.length()) {
+                merge += word1[i];
+                i++;
+            }
+
+            if (j < word2.length()) {
+                merge += word2[j];
+                j++;
+            }
+        }
+
+        return merge;
+    }
+};
