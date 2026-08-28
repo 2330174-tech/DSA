419be61fd45a3cc729bf2f9c1af0997a7c2f78eb
PRTKMR <2330174@kiit.ac.in>
2026-08-29 04:25:45 +0530

    Time: 4 ms (84.07%) | Memory: 23.5 MB (66.35%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    bool closeStrings(std::string word1, std::string word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        
+        std::vector<int> freq1(26, 0), freq2(26, 0);
+        
+        for (char c : word1) freq1[c - 'a']++;
+        for (char c : word2) freq2[c - 'a']++;
+        
+        // Check condition 2: Exact same set of unique characters present
+        for (int i = 0; i < 26; ++i) {
+            if ((freq1[i] == 0 && freq2[i] > 0) || (freq1[i] > 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+        
+        // Check condition 3: Exact same frequency distribution
+        std::sort(freq1.begin(), freq1.end());
+        std::sort(freq2.begin(), freq2.end());
+        
+        return freq1 == freq2;
+    }
+};
