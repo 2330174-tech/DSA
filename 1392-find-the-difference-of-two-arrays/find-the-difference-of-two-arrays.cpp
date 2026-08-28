67b5f35420e7e1fdafa6a152c12deb44c41c84de
PRTKMR <2330174@kiit.ac.in>
2026-08-29 04:23:30 +0530

    Time: 24 ms (43.99%) | Memory: 37.5 MB (48.46%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -4,26 +4,26 @@
 class Solution {
 public:
     std::vector<std::vector<int>> findDifference(std::vector<int>& nums1, std::vector<int>& nums2) {
-        // Store unique elements of both vectors into hash sets
+        // Build hash sets to remove duplicates and enable O(1) lookups
         std::unordered_set<int> set1(nums1.begin(), nums1.end());
         std::unordered_set<int> set2(nums2.begin(), nums2.end());
         
-        std::vector<int> ans1, ans2;
+        std::vector<int> diff1, diff2;
         
-        // Find elements in set1 that do not exist in set2
+        // Find elements in set1 that are not in set2
         for (int num : set1) {
-            if (set2.find(num) == set2.end()) {
-                ans1.push_back(num);
+            if (!set2.count(num)) {
+                diff1.push_back(num);
             }
         }
         
-        // Find elements in set2 that do not exist in set1
+        // Find elements in set2 that are not in set1
         for (int num : set2) {
-            if (set1.find(num) == set1.end()) {
-                ans2.push_back(num);
+            if (!set1.count(num)) {
+                diff2.push_back(num);
             }
         }
         
-        return {ans1, ans2};
+        return {diff1, diff2};
     }
 };
